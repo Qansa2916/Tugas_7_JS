b556c8fcb8a014f93cc979f3fca995ecc082f2f5
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 09:03:39 +0700

    Tugas_7_JS
    Tugas_7_JS

---
+++ Tugas-7.js
@@ -0,0 +1,13 @@
+//versi biasa
+var tinggi = [155, 150, 157, 160, 165, 158, 170, 175, 180, 140];
+for (i = 1; i < tinggi.length; i++) {
+    console.log(tinggi[i]);
+
+}
+console.log("");
+
+//versi For-of
+for (let x of tinggi) {
+    console.log(x);
+
+}
